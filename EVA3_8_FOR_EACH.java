e3d3c9ee5a83fac23d3eb269c100f4e7a79fc738
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-12-01 01:58:37 -0600

    Add files via upload

---
+++ EVA3_8_FOR_EACH.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_8_for_each;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA3_8_FOR_EACH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[]dias={"lunes","mates","miercoles","jueves","viernes","sabado","domingo"};
+        for(String cade:dias){
+         System.out.println(cade);
+     
+     }
+        int[]billetes={20,50,100,200,500,1000};
+        for(int billete:billetes )
+            System.out.println(billetes);
+        
+   }        
+  }
